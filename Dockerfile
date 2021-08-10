ef357c311c5a8d8eed90edd7e1e248996042c4b9
Matthew Trotter <matthew.trotter1@gmail.com>
2021-08-10 08:58:13 -0400

    secret key management

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM python:3
 ENV PYTHONUNBUFFERED=1
 COPY .aws /root/.aws
 WORKDIR /code
-COPY iot_camera_server/requirements.txt /code/iot_camera_server
+COPY iot_camera_server/requirements.txt /code/iot_camera_server/requirements.txt
 RUN pip install -r iot_camera_server/requirements.txt
 COPY . /code/
--- iot_camera_server/iot_camera_server/settings.py
+++ iot_camera_server/iot_camera_server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5ihtpz8cdts4pi-z!i&n-^_3iv3zbs)jy=no$79cz%utvp=x50'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'http://remoteiotcamera-env-1.eba-samhe3xp.us-east-1.elasticbeanstalk.com/',
+    'localhost'
+    ]
 
 
 # Application definition
